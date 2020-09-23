1b93e350f2c250c7e15a285fa4f66f6c4dd5d7b1
Dasha Lary <dk2800@nyu.edu>
2020-09-23 23:56:24 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist 
+attr_accessor :name, :song, :genre
+@@all = []
+
+def initialize(name)
+@name = name 
+@genre = genre
+@@all << self 
+end
+  
+def self.all 
+@@all 
+end
+
+def songs 
+Song.all.select {|song| song.artist == self}
+end
+
+def new_song(name, genre)
+Song.new(name, self, genre)
+end
+
+def genres
+songs.select {|song| song.genre == genre}
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
