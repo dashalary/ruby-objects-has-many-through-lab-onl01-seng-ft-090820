a832f99fdc73bd182af18f10501ea40e2ca3d17a
Dasha Lary <dk2800@nyu.edu>
2020-09-24 00:09:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,16 @@
 class Patient 
-  attr_accessor :name 
-  @@all = []
+attr_accessor :name 
+@@all = []
   
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end
+def initialize(name)
+@name = name 
+@@all << self 
+end
+  
+def self.all 
+  @@all 
+end
+  
   
   
   
